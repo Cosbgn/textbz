acbf892482f1b9d40261322afd5a9bff1209eb5a
Costantin Bignoli <cosbgn@gmail.com>
2018-01-30 18:18:12 +0100

    added background and graphics

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+var Home = { template: '<div>Home</div>' }
+var Foo = { template: '<div>Foo</div>' }
+
+
+var router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/:number', component: Home },
+    { path: '/foo', component: Foo }
+  ]
+})
+
+
 var app = new Vue({
   el: '#vue-home',
   // delimiters: ["{","}"],
