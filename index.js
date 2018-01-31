1cd3b8157068aa8022dcfd5c9cc81bb8b72bf4d9
Costantin Bignoli <cosbgn@gmail.com>
2018-01-31 11:00:37 +0100

    added decoding

--- index.js
+++ index.js
@@ -1,38 +1,37 @@
 var app = new Vue({
     el: '#vue-home',
     data: {
-        code:'',
         phoneNumber:'',
         },
 
-    created(){
-        var phoneNumber = window.location.pathname.substr(1) // The /
-        this.redirectNow(phoneNumber)
-    },
+    // created(){
+    //     var phoneNumber = window.location.pathname.substr(1) // The /
+    //     this.redirectNow(phoneNumber)
+    // },
 
-    methods:{
-        redirectNow: function(phoneNumber){
-
-            //var cleanPhoneNumber = phoneNumber.replace("-", "") // Remove all -
-            this.phoneNumber = phoneNumber
-            if (phoneNumber.length > 6 && phoneNumber != 'brand') {
-                var phoneNumber = phoneNumber.replace("-", "") // Remove all -
-                // Number could be valid:
-                console.log('Number Valid')
-                if(phoneNumber.startsWith('+')){
-                    var url = 'https://api.whatsapp.com/send?phone=' + phoneNumber.substr(1)
-                    window.location.replace(url)
-                }
-                else if(phoneNumber.startsWith(00)){
-                    var url = 'https://api.whatsapp.com/send?phone=' + phoneNumber.substr(2)
-                    window.location.replace(url)
-                } else {
-                    var url = 'https://api.whatsapp.com/send?phone=' + phoneNumber
-                    window.location.replace(url)
-                }
-            }
-        }
-    },
+    // methods:{
+    //     redirectNow: function(phoneNumber){
+    //
+    //         //var cleanPhoneNumber = phoneNumber.replace("-", "") // Remove all -
+    //         this.phoneNumber = phoneNumber
+    //         if (phoneNumber.length > 6 && phoneNumber != 'brand') {
+    //             var phoneNumber = phoneNumber.replace("-", "") // Remove all -
+    //             // Number could be valid:
+    //             console.log('Number Valid')
+    //             if(phoneNumber.startsWith('+')){
+    //                 var url = 'https://api.whatsapp.com/send?phone=' + phoneNumber.substr(1)
+    //                 window.location.replace(url)
+    //             }
+    //             else if(phoneNumber.startsWith(00)){
+    //                 var url = 'https://api.whatsapp.com/send?phone=' + phoneNumber.substr(2)
+    //                 window.location.replace(url)
+    //             } else {
+    //                 var url = 'https://api.whatsapp.com/send?phone=' + phoneNumber
+    //                 window.location.replace(url)
+    //             }
+    //         }
+    //     }
+    // },
 
     computed: {
       finalUrl: function(){
