0f841a935622bcaf0663bde483c40aa00dba075a
Costantin Bignoli <cosbgn@gmail.com>
2018-03-15 16:21:41 +0100

    adding progressive app

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ var app = new Vue({
     el: '#vue-home',
     data: {
         phoneNumber:'',
+		itsBrowser:true,
         },
+	created(){
+		var path = window.location.pathname;
+		if (path.indexOf("app") !== -1){
+			this.itsBrowser = false
+		}
+	},
 
     computed: {
       finalUrl: function(){
